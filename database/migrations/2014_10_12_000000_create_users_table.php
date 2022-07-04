b315c0f1e6e8a1db1b04e341d76cf5232d8b44f5
mateusoliiveira <teusolive@gmail.com>
2022-07-04 18:56:18 -0300

    Fix: role migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name', 100);
-            $table->tinyInteger('role')->default(0);
+            $table->integer('role')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
