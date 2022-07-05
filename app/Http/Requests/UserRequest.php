cb2ff4eee3b6da20bfeccb6b7de0fb9a62a2ee15
mateusoliiveira <teusolive@gmail.com>
2022-07-05 10:44:58 -0300

    Fix: register 3

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -18,7 +18,7 @@ class UserRequest extends FormRequest
 
     public function rules()
     {
-        if($this->method() != 'POST' || $this->method() != 'PATCH') return [];
+        if($this->method() != 'POST' && $this->method() != 'PATCH') return [];
 
         return [
             'email' => 'required|string|email|min:10|max:50|unique:users,email',
